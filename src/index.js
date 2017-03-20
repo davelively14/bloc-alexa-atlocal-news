7e4a590ebb8684b231ba11b07cfeac413ae2e500
Dave Lively <davelively14@gmail.com>
2017-03-20 17:54:58 -0400

    Added index.js and package.json

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var Alexa = require("alexa.sdk");
+var APP_ID = undefined;
+
+exports.handler = function(event, context, _callback) {
+  var alexa = Alexa.handler(event, context);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+};
+
+var handlers = {
+  "LaunchRequest": function() {
+    this.emit("GetHeadline");
+  },
+  "GetNewHeadlineIntent": function() {
+    this.emit("GetHeadline");
+  },
+  "GetHeadline": function() {
+
+  }
+};
