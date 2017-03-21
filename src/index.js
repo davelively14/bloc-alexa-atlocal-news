bfeae5114677c5e181f85d91c346c19585a1671b
Dave Lively <davelively14@gmail.com>
2017-03-20 21:39:48 -0400

    Test works, added alexa-sdk to the package.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-var Alexa = require("alexa.sdk");
-var APP_ID = undefined;
+"use strict";
+var Alexa = require("alexa-sdk");
+var APP_ID = "amzn1.ask.skill.0c8c8f3d-e08a-4a7c-8606-4cf338a389d0";
 
 exports.handler = function(event, context, _callback) {
   var alexa = Alexa.handler(event, context);
   alexa.APP_ID = APP_ID;
   alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var handlers = {
@@ -15,6 +17,6 @@ var handlers = {
     this.emit("GetHeadline");
   },
   "GetHeadline": function() {
-    
+    this.emit(":tell", "This is a test");
   }
 };
